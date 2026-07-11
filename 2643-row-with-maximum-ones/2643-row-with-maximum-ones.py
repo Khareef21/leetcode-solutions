99a358400bb6c3f6e5160d3d84ecbbc6ad42933c
khareef shaik <24R01A66Y0@cmrithyderabad.edu.in>
2026-07-12 01:58:28 +0530

    Time: 11 ms (45.66%), Space: 20 MB (75.85%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -1,16 +1,10 @@
-#brute force approach with O(n*m) time complexity
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        n=len(mat)
-        m=len(mat[0])
-        ind=-1
-        max_count=-1
-        for i in range(n):
-            count_row=0
-            for j in range(m):
-                count_row+=mat[i][j]
-            if count_row>max_count:
-                max_count=max(max_count,count_row)
-                ind=i
-        return [ind,max_count]
-        
+        max_count = 0
+        index = 0
+        for i in range(len(mat)):
+            count = sum(mat[i])
+            if count > max_count:
+                max_count = count
+                index = i
+        return [index, max_count]
