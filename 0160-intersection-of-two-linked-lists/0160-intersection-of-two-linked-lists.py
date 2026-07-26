fe1b5f37384362687085c5a322a6eaca356f848e
khareef shaik <24R01A66Y0@cmrithyderabad.edu.in>
2026-07-26 21:06:35 +0530

    Time: 116 ms (39.93%), Space: 38.1 MB (53.75%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,37 +6,17 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-
-        if headA == None or headB == None:
-            return None
-
-        curr1 = headA
-        len_1 = 1
-        while curr1.next != None:
-            len_1 += 1
-            curr1 = curr1.next
-
-        curr2 = headB
-        len_2 = 1
-        while curr2.next != None:
-            len_2 += 1
-            curr2 = curr2.next
-
-        curr1 = headA
-        curr2 = headB
-
-        if len_1 > len_2:
-            diff = len_1 - len_2
-            for i in range(diff):
-                curr1 = curr1.next
-        else:
-            diff = len_2 - len_1
-            for i in range(diff):
-                curr2 = curr2.next
-
-        while curr1 != None and curr2 != None:
-            if curr1 == curr2:
-                return curr1
-            curr1 = curr1.next
-            curr2 = curr2.next
-        return None
+        curr1=headA
+        curr2=headB
+        while curr1!=curr2:
+            if curr1==None:
+                curr1=headB
+            else:
+                curr1=curr1.next
+            
+            if curr2==None:
+                curr2=headA
+            else:
+                curr2=curr2.next
+        return curr1
+        
